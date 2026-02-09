f375bc274a9b2db0612cc9a76cf4174252ff4327
Aureusa <ppaureus@gmail.com>
2026-02-10 00:43:10 +0100

    removed value added cutout generation as it was replaced with CutoutCatalogue and AstroObject instead

--- src/core/cutout_maker/__init__.py
+++ src/core/cutout_maker/__init__.py
@@ -1,3 +1,3 @@
 from .finder import find_candidate_mosaics, chose_from_candidate_mosaics
 from .cutout import Cutout
-from .make_cutout import make_cutout, make_value_added_cutout
+from .make_cutout import make_cutout
--- src/core/cutout_maker/make_cutout.py
+++ src/core/cutout_maker/make_cutout.py
@@ -1,6 +1,5 @@
 from ..mosaic import Mosaic
 from .cutout import Cutout
-from .value_added_cutout import ValueAddedCutout
 from .astro_object import AstroObject
 
 def make_cutout(mosaic: Mosaic, ra: float, dec: float, size_arcmin: float = None, size_pixels: int = None):
@@ -15,35 +14,6 @@ def make_cutout(mosaic: Mosaic, ra: float, dec: float, size_arcmin: float = None
     :return: Cutout object
     """
     cutout = Cutout(mosaic, ra, dec, size_arcmin=size_arcmin, size_pixels=size_pixels)
-    if cutout.valid:
-        return cutout
-    else:
-        return None
-
-def make_value_added_cutout(
-    mosaic: Mosaic,
-    ra: float,
-    dec: float,
-    size_arcmin: float = None,
-    size_pixels: int = None,
-    ):
-    """
-    Create a ValueAddedCutout object for the given mosaic at specified RA, Dec.
-
-    :param mosaic: Mosaic object
-    :param ra: Right Ascension in degrees
-    :param dec: Declination in degrees
-    :param size_arcmin: Size of the cutout in arcminutes (optional)
-    :param size_pixels: Size of the cutout in pixels (optional)
-    :return: ValueAddedCutout object
-    """
-    cutout = ValueAddedCutout(
-        mosaic,
-        ra,
-        dec,
-        size_arcmin=size_arcmin,
-        size_pixels=size_pixels,
-    )
     if cutout.valid:
         return cutout
     else:
